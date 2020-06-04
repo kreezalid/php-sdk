b398e069d4136d85b97e1e6ff29aaab4a235cce4
Mohand Bencherif <mohand@n-sens.com>
2020-06-04 13:58:07 +0200

    Return only requested fields

--- src/Librairies/ApiBase.php
+++ src/Librairies/ApiBase.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace Kreezalid\Librairies;
-
 
 use Exception;
 
@@ -10,17 +8,23 @@ class ApiBase
 {
     public function castResponseToEntity($response, $entityClassName)
     {
-
         if (is_string($entityClassName)) {
             $entity = new $entityClassName();
         } else {
             throw new Exception("Cannot cast response to entity object. Wrong entity class name");
         }
 
-        $responseProperties = array_keys($response);
-        foreach ($responseProperties as $responseProperty){
-            $entity->{$responseProperty} = $response[$responseProperty];
+        $entityProperties = array_keys(get_object_vars($entity));
+
+        // Send back only responded properties (in case we use the fields param to filter only desired fields
+        foreach ($entityProperties as $field) {
+            if(isset($response[$field])) {
+                $entity->{$field} = $response[$field];
+            } else {
+                unset($entity->{$field});
+            }
         }
+
         return $entity;
     }
 
